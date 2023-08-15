0407507548a6424fc1dad3673061d2e7e1983991
wojlin <wojciech.linowski.2137@gmail.com>
2023-08-15 20:43:54 +0200

    broke AudioInfoData into .cpp and .h file

--- engine/include/GameData/AudioInfoData.h
+++ engine/include/GameData/AudioInfoData.h
@@ -1,12 +1,16 @@
-#include <iostream>
-#include <vector>
-#include <sstream>
-#include <string>
+#ifndef AUDIOINFODATA_H
+#define AUDIOINFODATA_H
 
-#ifndef AUDIODATA_H
-#define AUDIODATA_H
+#include <map>
+#include <math.h>
+#include <stdint.h>
+#include <string>
+#include <vector>
+#include <iostream>
+
 #include "Interfaces.h"
-#endif
+#include "WadStructure.h"
+
 
 struct voiceData_t
 {
@@ -51,59 +55,17 @@ class AudioInfoData: public baseResourceWAD
 {
     public:
 
-        AudioInfoData()
-        {
-        }
+        AudioInfoData();
 
-        AudioInfoData(WADStructure *wad)
-        {
-            filePath = wad->filePath;
-            GENMIDILump = wad->findLump("GENMIDI");
-            DMXGUSLump = wad->findLump("DMXGUSC");
-            readGENMIDIData();
-            readDMXGUSData();
-        }
+        AudioInfoData(WADStructure *wad);
 
-        void printInfo()
-        {
-            cout << endl << "|GENMIDI|" << endl;
-            for(int i = 0; i < midiData.size(); i++)
-            {
-                cout << i << ":  flags:" << midiData[i].flags << ",   tuning:" << unsigned(midiData[i].tuning) << ",   note:" << unsigned(midiData[i].note) << endl;
-            }
+        void printInfo();
 
-            //cout << endl << "|GENMIDI NAMES|" << endl;
-            //for(int i = 0; i < instrumentsData.size(); i++)
-            //{
-            //    cout << i << ": '" << instrumentsData[i] << "'" << endl;
-            //}
+        vector<midiData_t> getMIDIData();
 
-            cout << endl << "|DMXGUSC|" << endl;
-            for (const auto &data : dmxData)
-            {
-                std::cout << "" << data.patch
-                        << ", " << static_cast<int>(data.column256K)
-                        << ", " << static_cast<int>(data.column512K)
-                        << ", " << static_cast<int>(data.column768K)
-                        << ", " << static_cast<int>(data.column1024K)
-                        << ", " << data.patchName << std::endl;
-            }
-        }
+        midiData_t getMIDIDataByIndex(int index);
 
-        vector<midiData_t> getMIDIData()
-        {
-            return midiData;
-        }
-
-        midiData_t getMIDIDataByIndex(int index)
-        {
-            return midiData[index];
-        }
-
-        vector<DMXData_t> getDMXData()
-        {
-            return dmxData;
-        }
+        vector<DMXData_t> getDMXData();
 
     private:
         string filePath;
@@ -119,114 +81,13 @@ class AudioInfoData: public baseResourceWAD
 
         vector<DMXData_t> dmxData;
 
-        void readGENMIDIData()
-        {
-            std::ifstream file(filePath, std::ios::binary);
-            file.seekg(GENMIDILump.filepos);
-            char header[8];
+        void readGENMIDIData();
 
-            file.read(reinterpret_cast<char*>(&header), sizeof(header));  
+        int safe_stoi(const std::string& str);
 
-            for(int i =0; i < INSTRUMENTS_AMOUNT; i++)
-            {
-                midiData_t data;
+        void trim(std::string& str);
 
-                file.read(reinterpret_cast<char*>(&data), sizeof(data)); 
-
-                midiData.push_back(data);
-
-            }
-
-            for(int i =0; i < INSTRUMENTS_AMOUNT; i++)
-            {
-                char name[32];
-                file.read(reinterpret_cast<char*>(&name), sizeof(name)); 
-                instrumentsData.push_back(name);
-            }
-
-            file.close();
-
-        }
-
-        int safe_stoi(const std::string& str)
-        {
-            try {
-                return std::stoi(str);
-            } catch (const std::invalid_argument&) {
-                // Return 0 for empty strings, you can choose another default value if needed
-                return 0;
-            }
-        }
-
-        void trim(std::string& str)
-        {
-            const std::string whitespace = " \t";
-            const auto start = str.find_first_not_of(whitespace);
-            if (start != std::string::npos)
-            {
-                const auto end = str.find_last_not_of(whitespace);
-                str = str.substr(start, end - start + 1);
-            }
-            else
-            {
-                str.clear();
-            }
-        }
-
-        void readDMXGUSData()
-        {
-            int size = DMXGUSLump.size;
-            char data[DMXGUSLump.size];
-
-            std::ifstream file(filePath, std::ios::binary);
-            file.seekg(DMXGUSLump.filepos);
-            file.read(reinterpret_cast<char*>(&data), sizeof(data));
-            data[size] = '\0';
-            file.close();
-
-            std::istringstream iss(data);
-            std::string line;
-
-            // Skip the comment line
-            std::getline(iss, line);
-
-            // Skip the header line and extract column names
-            std::getline(iss, line);
-            std::istringstream iss_header(line);
-            std::string columnHeader;
-            for (int i = 0; i < 5; ++i) {
-                std::getline(iss_header, columnHeader, ' ');
-            }
-
-            while (std::getline(iss, line))
-            {
-                if (line.empty() || line[0] == '#')
-                {
-                    continue; // Skip comment lines
-                }
-
-                std::istringstream iss_line(line);
-                DMXData_t data;
-
-                std::string patch, column256K, column512K, column768K, column1024K, patchName;
-                std::getline(iss_line, patch, ',');
-                std::getline(iss_line, column256K, ',');
-                std::getline(iss_line, column512K, ',');
-                std::getline(iss_line, column768K, ',');
-                std::getline(iss_line, column1024K, ',');
-                std::getline(iss_line, patchName);
-
-                trim(patchName);
-
-                data.patch = safe_stoi(patch);
-                data.column256K = safe_stoi(column256K);
-                data.column512K = safe_stoi(column512K);
-                data.column768K = safe_stoi(column768K);
-                data.column1024K = safe_stoi(column1024K);
-                data.patchName = patchName;
-                
-
-                dmxData.push_back(data);
-            } 
-        }
+        void readDMXGUSData();
 };
+
+#endif
